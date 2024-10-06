4de0e66dc6fbe4527b2264c3d12d049768aa6771
andrewtubalinal <johnandrew@lapapa88.com>
2024-10-06 18:01:38 +0800

    added dependencies

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EM Furniture | Web</title>
-    <link rel="stylesheet" href="gulp.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="master.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link rel="stylesheet" href="../node_modules/slick-carousel/slick/slick.css">
