056f1ee89d60463a2b72cf261111867e221467e8
andrewtubalinal <johnandrew@lapapa88.com>
2024-10-12 06:22:35 +0800

    gulp

--- gulp.js
+++ gulp.js
@@ -1,21 +1,21 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
+// const gulp = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+// const concat = require('gulp-concat');
 
-gulp.task('sass', function() {
-  return gulp.src('mobile/keyframes.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('mobile')); 
-});
+// gulp.task('sass', function() {
+//   return gulp.src('mobile/keyframes.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('mobile')); 
+// });
 
-gulp.task('concat', function() {
-  return gulp.src([
-    'mobile/keyframes.css',
-    'notice.css', 
-    // Add paths to any other CSS files here
-  ])
-    .pipe(concat('master.css'))
-    .pipe(gulp.dest('')); // Output to project root
-});
+// gulp.task('concat', function() {
+//   return gulp.src([
+//     'mobile/keyframes.css',
+//     'notice.css', 
+//     // Add paths to any other CSS files here
+//   ])
+//     .pipe(concat('master.css'))
+//     .pipe(gulp.dest('')); // Output to project root
+// });
 
-gulp.task('default', gulp.parallel('sass', 'concat'));
+// gulp.task('default', gulp.parallel('sass', 'concat'));
